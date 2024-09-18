c71cecf27c0c613159425f9dd62ec3c888fccbfc
Elias <eliasmapendo97@gmail.com>
2024-09-17 19:46:37 -0700

    local database connection added

--- src/main/java/org/swe_group/ice_tracker/IceTrackerApplication.java
+++ src/main/java/org/swe_group/ice_tracker/IceTrackerApplication.java
@@ -1,13 +1,14 @@
 package org.swe_group.ice_tracker;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class IceTrackerApplication {
-    //instance:
-    //instance id:
-    //password:
+    //project name: Ice Tracker
+    //instance id: icetracker-instance
+    //password: R`6i*A#.{m-K3)a~
 
     public static void main(String[] args) {
         SpringApplication.run(IceTrackerApplication.class, args);
